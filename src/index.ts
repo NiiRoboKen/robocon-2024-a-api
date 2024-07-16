ea18cdee1c6851d78a13f5214bc227090b75fe01
らら <emptemp0000@gmail.com>
2024-07-16 18:26:16 +0900

    style: 💄 add prettier eslint & do format

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (request: Request, response: Response) => { 
+app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
-}); 
-
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  // gracefully handle error
-  throw new Error(error.message);
 });
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+  });
