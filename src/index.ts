eb81ca842b623dca66be05b65ddc0a29e43a7714
RRiverWest <kawa.ryo.0126@gmail.com>
2024-09-24 19:38:16 +0900

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { init } from "raspi";
 import { Serial } from "raspi-serial";
+import { cors } from "cors";
 const { Buffer } = require('node:buffer');
 
 dotenv.config();
@@ -16,6 +17,8 @@ let BACK_BOTTON_VOLUME = -127;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 type SerialData = {
   x: number,
@@ -56,9 +59,9 @@ type Turn = "left" | "right";
 
 type ControllerData = {
   event: Event,
-  
+
   collect: boolean,
-  
+
   left_firing: boolean,
   right_firing: boolean,
 };
@@ -97,14 +100,14 @@ init(() => {
           rightFiring: data.right_firing
         }
       };
-      
-      let flags = 0; 
+
+      let flags = 0;
       if (serialData!.flags.collect) { flags |= (1 << 7); }
       if (serialData!.flags.rightFiring) { flags |= (1 << 6); }
       if (serialData!.flags.leftFiring) { flags |= (1 << 5); }
       if (serialData!.flags.leftTurn) { flags |= (1 << 4); }
       if (serialData!.flags.rightTurn) { flags |= (1 << 3); }
-      
+
       const sendBuffer = Buffer.from([0x02, 0x03, serialData.x, serialData.y, flags, 0x03]);
       console.log(sendBuffer);
       serial.write(sendBuffer, () => {
